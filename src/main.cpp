09d2c3cff5ff76b0f788acd8e7332220294ec9a8
kuromamekuromame88 <tei8sia@outlook.jp>
2026-03-10 19:13:09 +0900

    あ

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 #include <Preferences.h>
+
+#define test
+
+#ifdef main
+
 #include "switch_ESP32.h"
 
 NSGamepad Gamepad;
@@ -194,3 +199,46 @@ void loop(){
 
   delay(5);
 }
+#endif
+
+#ifdef test
+#include <BleConnectionStatus.h>
+#include <BleGamepad.h>
+#include <BleGamepadConfiguration.h>
+#include <BleNUS.h>
+#include <BleOutputReceiver.h>
+
+BleGamepad bleGamepad;
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Starting BLE work!");
+    bleGamepad.begin();
+    // The default bleGamepad.begin() above enables 16 buttons, all axes, one hat, and no simulation controls or special buttons
+}
+
+void loop()
+{
+    if (bleGamepad.isConnected())
+    {
+        Serial.println("Press buttons 5, 16 and start. Move all enabled axes to max. Set DPAD (hat 1) to down right.");
+        bleGamepad.press(BUTTON_5);
+        bleGamepad.press(BUTTON_16);
+        bleGamepad.pressStart();
+        bleGamepad.setAxes(32767, 32767, 32767, 32767, 32767, 32767, 32767, 32767);       //(X, Y, Z, RX, RY, RZ)
+        //bleGamepad.setHIDAxes(32767, 32767, 32767, 32767, 32767, 32767, 32767, 32767);  //(X, Y, Z, RZ, RX, RY)
+        bleGamepad.setHat1(HAT_DOWN_RIGHT);
+        // All axes, sliders, hats etc can also be set independently. See the IndividualAxes.ino example
+        delay(500);
+
+        Serial.println("Release button 5 and start. Move all axes to min. Set DPAD (hat 1) to centred.");
+        bleGamepad.release(BUTTON_5);
+        bleGamepad.releaseStart();
+        bleGamepad.setHat1(HAT_CENTERED);
+        bleGamepad.setAxes(0, 0, 0, 0, 0, 0, 0, 0);           //(X, Y, Z, RX, RY, RZ)
+        //bleGamepad.setHIDAxes(0, 0, 0, 0, 0, 0, 0, 0);      //(X, Y, Z, RZ, RX, RY)
+        delay(500);
+    }
+}
+#endif
